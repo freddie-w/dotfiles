c4e974526762e27f7285729ee056c003dbaf5834
Freddie Wellfair <freddie.wellfair@gmail.com>
2026-04-02 21:38:09 +0100

    treesitter syntax highlighting

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -3,3 +3,8 @@ vim.pack.add {
 }
 
 require('nvim-treesitter').install { 'lua', 'javascript', 'html', 'tsx', 'css', 'json', 'dockerfile' }
+
+-- Enable syntax highlighting
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function() vim.treesitter.start() end,
+})
